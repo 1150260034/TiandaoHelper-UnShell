ff89149864494fb415c99cc965ec1566731110c9
1150260034 <1150260034@qq.com>
2026-03-30 21:57:39 +0800

    Add: test Python functions for review

---
+++ test_review.py
@@ -0,0 +1,12 @@
+def calculate(a, b):
+    """Calculate sum of two numbers."""
+    return a + b
+
+def multiply(a, b):
+    """Multiply two numbers."""
+    return a * b
+
+# Test
+if __name__ == "__main__":
+    print(calculate(1, 2))
+    print(multiply(3, 4))
